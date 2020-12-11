8ff9e0288693ebd5e667dd08574a5d9762fa0bb8
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-12-11 16:58:59 -0300

    primeiros passos

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,16 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+	theme: {
+		themes: {
+			light: {
+				primary: colors.grey.darken3, // #E53935
+				secondary: colors.yellow.base, // #FFCDD2
+			},
+		},
+	},
 });
