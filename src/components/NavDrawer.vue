40873c9c686c50a7f1188bef7cf1dc754229dd9b
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-12-15 17:42:02 -0300

    tree view funcionando

--- src/components/NavDrawer.vue
+++ src/components/NavDrawer.vue
@@ -1,11 +1,19 @@
 <template>
-	<v-navigation-drawer class="pt-4" permanent app clipped right color="#1c1f23">
+	<v-navigation-drawer
+		class="pt-4"
+		permanent
+		app
+		clipped
+		right
+		color="#1c1f23"
+		width="350px"
+	>
 		<v-treeview
 			v-model="tree"
-			:open="initiallyOpen"
 			:items="items"
 			activatable
 			item-key="name"
+			transition
 			open-on-click
 		>
 			<template v-slot:label="{ item }">
@@ -23,7 +31,14 @@ export default {
 	data: () => ({
 		items: [
 			{ name: "Overview" },
-			{ name: "Perfil do aluno" },
+			{
+				name: "Perfil do aluno",
+				children: [
+					{
+						name: "Quantidade de respostas",
+					},
+				],
+			},
 			{ name: "Sobre RAE" },
 		],
 		right: null,
