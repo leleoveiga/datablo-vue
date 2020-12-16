5111e570cbf6993ee966b293a0b0551f6a71942e
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-12-16 15:06:42 -0300

    update css header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,9 @@
 <template>
-	<v-app-bar
-		clipped-right
-		app
-		color="primary"
-		dark
-		scroll-off-screen
-		scroll-threshold="50"
-	>
+	<v-app-bar color="primary" absolute dark>
 		<v-icon color="#eead2d" size="36">mdi-database</v-icon>
 		<div class="d-flex align-center">
 			<p class="ma-0 display-1">Datablo</p>
+			<!-- <v-toolbar-title>Datablo</v-toolbar-title> -->
 		</div>
 
 		<v-spacer></v-spacer>
