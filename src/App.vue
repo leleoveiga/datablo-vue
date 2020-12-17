b12663b7b9c0baa84cfb5b8f8d8339aeaa466091
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-12-17 17:35:44 -0300

    scrollbar dark theme

--- src/App.vue
+++ src/App.vue
@@ -23,3 +23,28 @@ export default {
 	},
 };
 </script>
+
+<style>
+html {
+	--scrollbarBG: #1f1f1f;
+	--thumbBG: #4a4a4a;
+}
+
+body::-webkit-scrollbar {
+	width: 16px;
+}
+
+body::-webkit-scrollbar-track {
+	background: #1c1f23;
+}
+
+body::-webkit-scrollbar-thumb {
+	background-color: var(--thumbBG);
+	border-radius: 0px;
+	border: 0px solid var(--scrollbarBG);
+}
+
+body::-webkit-scrollbar-thumb:hover {
+	background: #575757;
+}
+</style>
