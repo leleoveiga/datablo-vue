c5eaf2b5a031c708b42481d4e36d4a862141ff0f
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-12-18 15:35:12 -0300

    refatorando declaração da fonte

--- src/components/NavDrawer.vue
+++ src/components/NavDrawer.vue
@@ -8,11 +8,6 @@
 		color="#1c1f23"
 		width="375px"
 	>
-		<link rel="preconnect" href="https://fonts.gstatic.com" />
-		<link
-			href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800&display=swap"
-			rel="stylesheet"
-		/>
 		<v-treeview
 			v-model="tree"
 			:items="items"
