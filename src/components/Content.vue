d3733f417a97f53f592299edf2ef10a482e7726b
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-12-26 22:32:59 -0300

    refatorando

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -62,151 +62,20 @@
 		<!-- </div> -->
 		<!-- <v-btn>Github</v-btn> -->
 
-		<strong class="mt-16" style="font-size: 2em"
-			>Perfil dos Respondentes</strong
-		>
-		<div class="textGraphs">
-			<p class="p1 grey--text text--lighten-4">
-				Ao todo, 1567 estudantes, o que corresponde a pouco menos de 10% do
-				total de alunos de graduação da UFCG, responderam à pesquisa de opinião
-				ao longo de duas semanas, dos quais a maior parte são dos campi de
-				Campina Grande, Cuité e Cajazeiras, que juntos representam mais de 75%
-				dos respondentes.
-			</p>
-			<p class="p1 grey--text text--lighten-4">
-				Além disso, é notável que a grande maioria dos participantes se
-				encontram nos períodos iniciais de seus cursos. Cerca de 43% deles ainda
-				não chegaram no 4º período letivo. Percebe-se, com isso, que o
-				engajamento nesta consulta tende a ser consideravelmente menor entre os
-				estudantes mais veteranos.
-			</p>
-		</div>
-		<!-- card dos gráficos -->
-		<v-card class="marginGraph" width="90%" dark>
-			<div
-				class="font-weight-bold body-1 d-flex align-center justify-center mt-10"
-			>
-				Quantidade de Respondentes
-			</div>
-			<div class="d-flex align-center justify-center mt-10">
-				<v-btn
-					class="mx-3"
-					color="#eead2dcc"
-					light
-					@click="tipoGraficoQuantidade = 0"
-					>Geral</v-btn
-				>
-				<v-btn
-					class="mx-3"
-					color="#eead2dcc"
-					light
-					@click="tipoGraficoQuantidade = 1"
-					>Por curso</v-btn
-				>
-				<v-btn
-					class="mx-3"
-					color="#eead2dcc"
-					light
-					@click="tipoGraficoQuantidade = 2"
-					>Por campus</v-btn
-				>
-			</div>
-			<!-- <v-expand-transition> -->
-			<QuantidadeGeral
-				v-if="tipoGraficoQuantidade === 0"
-				class="mt-n5"
-				height="1750"
-				width="900"
-			/>
-			<QuantidadePorCurso
-				v-else-if="tipoGraficoQuantidade === 1"
-				class="mt-n5"
-				height="485"
-				width="900"
-			/>
-			<QuantidadePorCampus
-				v-else-if="tipoGraficoQuantidade === 2"
-				class="mt-n5"
-				height="380"
-				width="900"
-			/>
-			<!-- </v-expand-transition> -->
-		</v-card>
-
-		<strong class="mt-16" style="font-size: 2em">Sobre o RAE</strong>
-
-		<div class="textGraphs">
-			<p class="p1 grey--text text--lighten-4">
-				Depois de questionados sobre diversos aspectos do RAE (Regime Acadêmico
-				Extraordinário), fica aparente a baixa popularidade do mesmo entre os
-				estudantes de graduação. Quando perguntada diretamente, por exemplo, sua
-				opinião geral sobre o sistema, 41% o classificaram como ruim ou péssimo
-				em contraste aos 21% que declararam opinião positiva.
-			</p>
-			<p class="p1 grey--text text--lighten-4">
-				Em questões mais específicas, a impopularidade do regime fica ainda mais
-				evidente: 71% dos respondentes avaliam a qualidade do ensino como pior,
-				em relação ao presencial. Além disso, 91% dos consultados declararam ter
-				tido, ao menos uma vez, algum tipo de problema técnico que dificultou ou
-				inviabilizou a realização de atividades nesse período. Destes, 79% dizem
-				ter seu desempenho afetado por esses imprevistos.
-			</p>
-		</div>
-
-		<v-card class="marginGraph" width="90%" dark>
-			<div
-				class="font-weight-bold body-1 d-flex align-center justify-center mt-10"
-			>
-				Você acredita que as vagas ofertadas no RAE foram suficientes?
-			</div>
-			<div class="d-flex align-center justify-center mt-10">
-				<v-btn
-					class="mx-3"
-					color="#eead2dcc"
-					light
-					@click="tipoGraficoVagas = 0"
-					>Geral</v-btn
-				>
-
-				<v-btn
-					class="mx-3"
-					color="#eead2dcc"
-					light
-					@click="tipoGraficoVagas = 1"
-					>Por Curso</v-btn
-				>
-			</div>
-			<VagasOfertadasGeral
-				v-if="tipoGraficoVagas === 0"
-				class=""
-				height="230"
-				width="900"
-			/>
-			<VagasOfertadasPorCurso
-				v-else-if="tipoGraficoVagas === 1"
-				class=""
-				height="500"
-				width="900"
-			/>
-		</v-card>
+		<PerfilRespondentes />
+		<SobreRae />
 	</v-card>
 </template>
 
 <script>
-import QuantidadeGeral from "./graphs/quantidade/QuantidadeGeral";
-import QuantidadePorCurso from "./graphs/quantidade/QuantidadePorCurso";
-import QuantidadePorCampus from "./graphs/quantidade/QuantidadePorCampus";
-import VagasOfertadasGeral from "./graphs/vagasOfertadas/VagasOfertadasGeral";
-import VagasOfertadasPorCurso from "./graphs/vagasOfertadas/VagasOfertadasPorCurso";
+import PerfilRespondentes from "./pages/PerfilRespondentes";
+import SobreRae from "./pages/SobreRae.vue";
 
 export default {
 	name: "Content",
 	components: {
-		QuantidadeGeral,
-		QuantidadePorCurso,
-		QuantidadePorCampus,
-		VagasOfertadasGeral,
-		VagasOfertadasPorCurso,
+		PerfilRespondentes,
+		SobreRae,
 	},
 	data: function() {
 		return {
---
+++ src/components/pages/SobreRae.vue
@@ -0,0 +1,79 @@
+<template>
+	<div class="d-flex flex-column align-center mt-1">
+		<strong class="mt-16" style="font-size: 2em">Sobre o RAE</strong>
+
+		<div class="textGraphs">
+			<p class="p1 grey--text text--lighten-4">
+				Depois de questionados sobre diversos aspectos do RAE (Regime Acadêmico
+				Extraordinário), fica aparente a baixa popularidade do mesmo entre os
+				estudantes de graduação. Quando perguntada diretamente, por exemplo, sua
+				opinião geral sobre o sistema, 41% o classificaram como ruim ou péssimo
+				em contraste aos 21% que declararam opinião positiva.
+			</p>
+			<p class="p1 grey--text text--lighten-4">
+				Em questões mais específicas, a impopularidade do regime fica ainda mais
+				evidente: 71% dos respondentes avaliam a qualidade do ensino como pior,
+				em relação ao presencial. Além disso, 91% dos consultados declararam ter
+				tido, ao menos uma vez, algum tipo de problema técnico que dificultou ou
+				inviabilizou a realização de atividades nesse período. Destes, 79% dizem
+				ter seu desempenho afetado por esses imprevistos.
+			</p>
+		</div>
+
+		<v-card class="marginGraph" width="90%" dark>
+			<div
+				class="font-weight-bold body-1 d-flex align-center justify-center mt-10"
+			>
+				Você acredita que as vagas ofertadas no RAE foram suficientes?
+			</div>
+			<div class="d-flex align-center justify-center mt-10">
+				<v-btn
+					class="mx-3"
+					color="#eead2dcc"
+					light
+					@click="tipoGraficoVagas = 0"
+					>Geral</v-btn
+				>
+
+				<v-btn
+					class="mx-3"
+					color="#eead2dcc"
+					light
+					@click="tipoGraficoVagas = 1"
+					>Por Curso</v-btn
+				>
+			</div>
+			<VagasOfertadasGeral
+				v-if="tipoGraficoVagas === 0"
+				class=""
+				height="230"
+				width="900"
+			/>
+			<VagasOfertadasPorCurso
+				v-else-if="tipoGraficoVagas === 1"
+				class=""
+				height="500"
+				width="900"
+			/>
+		</v-card>
+	</div>
+</template>
+
+<script>
+import VagasOfertadasGeral from "../graphs/vagasOfertadas/VagasOfertadasGeral";
+import VagasOfertadasPorCurso from "../graphs/vagasOfertadas/VagasOfertadasPorCurso";
+export default {
+	name: "SobreRae",
+	components: {
+		VagasOfertadasGeral,
+		VagasOfertadasPorCurso,
+	},
+	data: function() {
+		return {
+			tipoGraficoVagas: 0,
+		};
+	},
+};
+</script>
+
+<style></style>
