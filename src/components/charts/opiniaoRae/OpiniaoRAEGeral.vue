a6ffde3041428ddf164b00df18d6f167699c2e69
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-12-27 01:19:22 -0300

    add opiniao rae chart

---
+++ src/components/charts/opiniaoRae/OpiniaoRAEGeral.vue
@@ -0,0 +1,88 @@
+<script>
+import { HorizontalBar } from "vue-chartjs";
+
+export default {
+	name: "BarChart",
+	extends: HorizontalBar,
+	mounted() {
+		this.renderChart(
+			{
+				labels: [
+					"Excelente",
+					"Bom",
+					"Razoável",
+					"Ruim",
+					"Péssimo",
+					"Não Responderam",
+				],
+
+				datasets: [
+					{
+						data: [49, 287, 569, 352, 288, 22],
+						backgroundColor: "#EEAD2D",
+						barThickness: "flex",
+						hoverBackgroundColor: "#b8831c",
+					},
+				],
+			},
+			{
+				maintainAspectRatio: false,
+				layout: {
+					padding: 60,
+				},
+
+				legend: {
+					display: false,
+					/*                         labels: {
+                            fontSize: 15,
+                            fontColor: "#e6e7ea",
+                            fontFamily: 'Roboto',
+                        },
+        */
+				},
+
+				title: {
+					display: false,
+					padding: 20,
+					text: "No geral, você acha o RAE:",
+					fontSize: 25,
+					fontColor: "#e6e7ea",
+					fontFamily: "Roboto",
+				},
+
+				scales: {
+					yAxes: [
+						{
+							barPercentage: 1,
+
+							gridLines: {
+								display: false,
+							},
+
+							ticks: {
+								beginAtZero: true,
+								fontSize: 14,
+								fontColor: "#e6e7ea",
+								fontFamily: "Roboto",
+							},
+
+							barThickness: 23,
+						},
+					],
+
+					xAxes: [
+						{
+							display: false,
+							gridLines: {
+								display: false,
+							},
+						},
+					],
+				},
+			}
+		);
+	},
+};
+</script>
+
+<style></style>
