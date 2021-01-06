58cab7fd31f9da51096348d44eb31a52917dff2f
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2021-01-06 17:12:44 -0300

    add sobre professores

---
+++ src/components/pages/SobreProfessores/SobreProfessores.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="d-flex flex-column align-center mt-1">
+		<strong class="mt-16" style="font-size: 2em">Sobre os Professores</strong>
+
+		<div class="textGraphs">
+			<p class="p1 grey--text text--lighten-4">
+				Nesta seção, nós observamos a opinião dos estudantes sobre a atuação dos
+				professores no RAE. Foram consultados sobre diversos tópicos, como o
+				atendimento individual, tendo boa aceitação pela maioria dos estudantes
+				(61%), que têm opinião positiva ou neutra.
+			</p>
+			<p class="p1 grey--text text--lighten-4">
+				De modo geral, apesar de serem considerados mais exigentes (por 42% dos
+				respondentes), pedindo um volume de atividades mais alto que o normal,
+				dito abusivo por 52% dos alunos, os professores gozam de uma boa
+				avaliação geral, tendo opiniões positivas de 42% dos que responderam à
+				pesquisa. Vale destacar os cursos que melhor avaliaram seus professores:
+				Enfermagem, Matemática, História, Design e Ciência da Computação. E os
+				que pior avaliaram: Direito, Química, Medicina, Engenharia Elétrica e
+				Ciências Biológicas
+			</p>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
