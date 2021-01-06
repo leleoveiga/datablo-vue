dabc0ee6178a5e6fda2a5ae59236ba844cb68b50
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2021-01-06 17:12:35 -0300

    add exigencia professores

---
+++ src/components/charts/sobreProfessores/exigenciaProfessores/ExigenciaProfessoresGeral.vue
@@ -0,0 +1,86 @@
+<script>
+import { HorizontalBar } from "vue-chartjs";
+
+export default {
+	name: "BarChart",
+	extends: HorizontalBar,
+	mounted() {
+		this.renderChart(
+			{
+				labels: [
+					"Mais Exigentes",
+					"Tão Exigentes Quanto",
+					"Menos Exigentes",
+					"Não Responderam",
+				],
+
+				datasets: [
+					{
+						data: [659, 491, 264, 153],
+						backgroundColor: "#EEAD2D",
+						barThickness: "flex",
+						hoverBackgroundColor: "#b8831c",
+					},
+				],
+			},
+			{
+				maintainAspectRatio: false,
+				layout: {
+					padding: 60,
+				},
+
+				legend: {
+					display: false,
+					/*                         labels: {
+                            fontSize: 15,
+                            fontColor: "#e6e7ea",
+                            fontFamily: 'Roboto',
+                        },
+        */
+				},
+
+				title: {
+					display: false,
+					padding: 20,
+					text: "Os professores estão:",
+					fontSize: 25,
+					fontColor: "#e6e7ea",
+					fontFamily: "Roboto",
+				},
+
+				scales: {
+					yAxes: [
+						{
+							barPercentage: 1,
+
+							gridLines: {
+								display: false,
+							},
+
+							ticks: {
+								beginAtZero: true,
+								fontSize: 14,
+								fontColor: "#e6e7ea",
+								fontFamily: "Roboto",
+							},
+
+							barThickness: 23,
+						},
+					],
+
+					xAxes: [
+						{
+							display: false,
+							gridLines: {
+								display: false,
+							},
+						},
+					],
+				},
+			}
+		);
+	},
+};
+</script>
+
+<style></style>
---
+++ src/components/charts/sobreProfessores/exigenciaProfessores/ExigenciaProfessoresPorCampus.vue
@@ -0,0 +1,127 @@
+<script>
+import { HorizontalBar } from "vue-chartjs";
+
+export default {
+	name: "BarChart",
+	extends: HorizontalBar,
+	mounted() {
+		this.renderChart(
+			{
+				labels: [
+					"Mais Exigentes",
+					"Tão Exigentes Quanto",
+					"Menos Exigentes",
+					"Não Responderam",
+				],
+				datasets: [
+					{
+						label: "Campina Grande",
+						hidden: false,
+						data: [221, 197, 100, 57],
+						backgroundColor: "#eead2d",
+						barThickness: "flex",
+					},
+
+					{
+						label: "Cajazeiras",
+						hidden: true,
+						data: [79, 83, 51, 25],
+						backgroundColor: "#00F042",
+						barThickness: "flex",
+					},
+
+					{
+						label: "Cuité",
+						hidden: true,
+						data: [164, 106, 60, 47],
+						backgroundColor: "#753bff",
+						barThickness: "flex",
+					},
+
+					{
+						label: "Patos",
+						hidden: true,
+						data: [32, 14, 9, 6],
+						backgroundColor: "#0017F0",
+						barThickness: "flex",
+					},
+
+					{
+						label: "Pombal",
+						hidden: true,
+						data: [84, 47, 20, 10],
+						backgroundColor: "#F00106",
+						barThickness: "flex",
+					},
+
+					{
+						label: "Sousa",
+						hidden: true,
+						data: [77, 41, 24, 7],
+						backgroundColor: "#cf13cb",
+						barThickness: "flex",
+					},
+				],
+			},
+			{
+				maintainAspectRatio: false,
+				align: "center",
+				layout: {
+					padding: 60,
+				},
+
+				legend: {
+					display: true,
+					labels: {
+						fontSize: 13,
+						fontColor: "#e6e7ea",
+						fontFamily: "Roboto",
+						boxWidth: 15,
+					},
+				},
+
+				title: {
+					display: false,
+					padding: 20,
+					text: "por Campi",
+					fontSize: 25,
+					fontColor: "#e6e7ea",
+					fontFamily: "Roboto",
+				},
+
+				scales: {
+					yAxes: [
+						{
+							barPercentage: 0.9,
+
+							gridLines: {
+								display: false,
+							},
+
+							ticks: {
+								beginAtZero: true,
+								fontSize: 14,
+								fontColor: "#e6e7ea",
+								fontFamily: "Roboto",
+							},
+
+							barThickness: "flex",
+						},
+					],
+
+					xAxes: [
+						{
+							display: false,
+							gridLines: {
+								display: false,
+							},
+						},
+					],
+				},
+			}
+		);
+	},
+};
+</script>
+
+<style></style>
